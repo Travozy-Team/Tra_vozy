31e4d23219be5d9b2505edbd70959563a213eab6
nahian afroz <nahianafroz0@gmail.com>
2025-09-02 19:58:31 +0600

    featured package section

--- Tra_vozy/static/js/script.js
+++ Tra_vozy/static/js/script.js
@@ -268,3 +268,28 @@ themeToggle.addEventListener('change', function () {
         localStorage.removeItem('dark-mode');
     }
 });
+
+
+// Packages Swiper
+document.addEventListener('DOMContentLoaded', function () {
+    // Card Packages Swiper
+    const cardSwiper = new Swiper('.container.swiper', {
+        loop: true,
+        spaceBetween: 30,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+            dynamicBullets: true
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            0: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 }
+        },
+    });
+
+});
